5a875e1077d5a41aaaa176e8a5df90cc32392ae3
Seyoon <psyoona@naver.com>
2023-06-03 12:07:23 +0900

    Ver 1.00.000 Vue 변수 타입별 처리 방식 추가

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,9 +1,43 @@
-﻿const { createApp } = Vue
-
-createApp({
+﻿Vue.createApp({
 	data() {
 		return {
-			message: 'Hello Vue!'
+			message: 'Hello Vue!',
+			string1: '문자열',
+			int1: 123,
+			float1: 1.11
 		}
 	}
 }).mount('#app');
+
+Vue.createApp({
+	data() {
+		return {
+			message: 'Hello Vue!',
+			string1: '문자열',
+			int1: 123,
+			float1: 1.11
+		}
+	}
+}).mount('#app2');
+
+var test = Vue.createApp({
+	data() {
+		return {
+			message: 'Hello Vue!',
+			string1: '문자열',
+			int1: 123,
+			float1: 1.11,
+			boolean1: true,
+			array1: [50, 60, 70],
+			object1: {
+				a1: 'a1 문자열',
+				a2: 'a2 문자열'
+			}
+		}
+	},
+	methods: {
+		userInfo: () => {
+			console.log('hihihi');
+		}
+	}
+}).mount('#app3');
