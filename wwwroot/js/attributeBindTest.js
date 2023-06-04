8caeb4607afd4e803ba4a9a7028685ddf650d544
Seyoon <psyoona@naver.com>
2023-06-04 21:03:33 +0900

    css 바인딩 내용 추가

--- wwwroot/js/attributeBindTest.js
+++ wwwroot/js/attributeBindTest.js
@@ -4,7 +4,11 @@ let attributeBindTest = Vue.createApp({
 			string1: '문자열',
 			link: 'http://yoonslab.com/Laboratory/Tetris',
 			imgsrc: '../images/logo.png',
-			imgWidth: '200'
+			imgWidth: '200',
+			css1Name: 'css1',
+			css2Name: 'css2',
+			isCss1: false,
+			isCss2: false
 		}
 	},
 	methods: {
@@ -15,6 +19,14 @@ let attributeBindTest = Vue.createApp({
 		resetImageWidth: function () {
 			this.imgWidth = '200';
 			console.log('resetImageWidth');
+		},
+		setCss1: function () {
+			this.isCss1 = true;
+			this.isCss2 = true;
+		},
+		removeCss: function () {
+			this.isCss1 = false;
+			this.isCss2 = false;
 		}
 	}
 }).mount('#attributeBindTest');
