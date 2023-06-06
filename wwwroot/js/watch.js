70ca96ec1f07db139539a32eff184c195438586e
Seyoon <psyoona@naver.com>
2023-06-06 16:12:33 +0900

    arrow 문법 제거

--- wwwroot/js/watch.js
+++ wwwroot/js/watch.js
@@ -13,14 +13,14 @@ let watchVue = Vue.createApp({
 		// 변수 a1을 감시하는 watch 설정
 		a1: function(newValue, oldValue) {
 			console.log(`a1의 새로운 값: ${newValue}, 이전 값: ${oldValue}`);
-			watchVue.a2 = `a1 : ${newValue}`;
+			this.a2 = `a1 : ${newValue}`;
 		},
-		a2: (newValue, oldValue) => {
+		a2: function(newValue, oldValue) {
 			console.log(`a2의 새로운 값: ${newValue}, 이전 값: ${oldValue}`);
 			
-			watchVue.a3 = `a2 : ${newValue}`;
+			this.a3 = `a2 : ${newValue}`;
 		},
-		a3: (newValue, oldValue) => {
+		a3: function (newValue, oldValue) {
 			console.log(`a3의 새로운 값: ${newValue}, 이전 값: ${oldValue}`);
 		}
 	}
